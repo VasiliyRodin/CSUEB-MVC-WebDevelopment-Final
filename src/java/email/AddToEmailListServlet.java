64da486f6da28ea63d0889d51529f322c454a98f
VasiliyRodin <rodinvasiliy@gmail.com>
2014-05-20 11:00:57 -0700

    Finished from powerPoint
    Need to see the fixes

--- src/java/email/AddToEmailListServlet.java
+++ src/java/email/AddToEmailListServlet.java
@@ -5,6 +5,7 @@ import data.UsersIO;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import org.apache.catalina.User;
 
 
 public class AddToEmailListServlet extends HttpServlet
@@ -19,5 +20,28 @@ public class AddToEmailListServlet extends HttpServlet
         // Create user Object
         User user = new User (firstName, lastName, email);
         
+        //validate the params
+        String message ="";
+        String url = "";
+        if(firstName.length() == 0 || lastName.length() == 0 || email.length() == 0)
+        {
+            message = "Please fill out all three text boxes.";
+            url = "/index.jsp";
+        }
+        else
+        {
+            message = "";
+            ServletContext context = getServletContext();
+            String path = context.getRealPath("/WEB-INF/EmailList.txt");
+            UserIO.addRecord(user,path);
+            
+            url = "/display_email_entry.jsp";
+        }
+        request.setAttribute("user",user);
+        request.setAttribute("message",message);
+        
+        //forwards request and response to the view
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request,response);
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -14,7 +14,7 @@
     <body>
         <h1>Vasiliy's email repository!!!</h1>
         <h1>Please enter all the information to sign up for our email list.</h1>
-        <form action="validateEntry" method="post">
+        <form action="AddToEmailListServlet" method="post">
             <table cellspacing="5" border="0">
                 <tr>
                     <td align="right">First Name:</td>
